c76365bcde611b10c544e1cca7f57c2bc717391d
Magdalena Sremska <magdalena.sremska@gmail.com>
2020-04-13 19:17:10 +0200

    Updates

---
+++ precache-manifest.86dc68e5ce832dfe844e8ab4dab991fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504311fae5d76e1882abf21f0ad327dc",
+    "url": "/Users-list/index.html"
+  },
+  {
+    "revision": "8c289b233cc9bad89f9e",
+    "url": "/Users-list/static/css/main.cff17b71.chunk.css"
+  },
+  {
+    "revision": "f8e16fb1730f3db41fe6",
+    "url": "/Users-list/static/js/2.91c02d53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Users-list/static/js/2.91c02d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c289b233cc9bad89f9e",
+    "url": "/Users-list/static/js/main.1b8ec232.chunk.js"
+  },
+  {
+    "revision": "31968cdf33815fc62251",
+    "url": "/Users-list/static/js/runtime-main.3b0ac7ca.js"
+  }
+]);
